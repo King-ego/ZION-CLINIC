bcd77edc1ac879381ec2ac0fbea0a686c12c21f1
King-ego <77419529+King-ego@users.noreply.github.com>
2021-05-06 00:44:06 -0300

    06/05 00:43

--- js/index.js
+++ js/index.js
@@ -1,44 +1,54 @@
-(function(){
+(function () {
     const containerImg = document.querySelector('.carrocelImg');
     const ArrayImg = containerImg.querySelectorAll('.biochectomia')
     const imgActive = document.querySelector('.biochectomia')
-    function selectEvent(e){
+    function selectEvent(e) {
         const el = e.target;
-        let n = 0;
-        let m =0;
-        if(el.classList.contains('rightImgCarrocel')){
-            for(let valor in ArrayImg){
-                console.log(ArrayImg.length, valor)
-                if(valor == ArrayImg.length-1){
-                    ArrayImg[valor].classList.add('active')
-                    break;
-                }
-                if(n===1){
-                    ArrayImg[valor].classList.add('active')
-                    break
-                }
-                if(ArrayImg[valor].classList.contains('active')){
-                    ArrayImg[valor].classList.remove('active')
-                     n = 1;
-                }
-            }
-        }
-        if(el.classList.contains('leftImgCarrocel')){
-            for(let i in ArrayImg){
+        let m = 0;
+        if (el.classList.contains('rightImgCarrocel')) {addClass()}
+        if (el.classList.contains('leftImgCarrocel')) {
+            for (let i in ArrayImg) {
                 console.log(ArrayImg.length, i)
-                if(i>=1 && ArrayImg[i].classList.contains('active')){
-                    if(i-1>=0){
+                if (i >= 0 && ArrayImg[i].classList.contains('active')) {
+                    if (i - 1 >= 0) {
                         ArrayImg[i].classList.remove('active');
                         m = 1
+                    } else {
+                        ArrayImg[i].classList.remove('active');
+                        ArrayImg[ArrayImg.length - 1].classList.add('active')
+                        break;
                     }
+
                 }
-                if(m===1){
-                    ArrayImg[i-1].classList.add('active')
+                if (m === 1) {
+                    ArrayImg[i - 1].classList.add('active')
                     return;
                 }
             }
             return;
         }
     }
-    containerImg.addEventListener('click',selectEvent);
+    function addClass(){
+        let n =0 ;
+            for (let valor in ArrayImg) {
+                console.log(ArrayImg.length, valor)
+                if (n === 1) {
+                    ArrayImg[valor].classList.add('active')
+                    break
+                }
+                if (ArrayImg[valor].classList.contains('active')) {
+                    ArrayImg[valor].classList.remove('active');
+                    n = 1;
+                }
+                if (valor == ArrayImg.length - 1) {
+                    ArrayImg[valor].classList.remove('active')
+                    ArrayImg[0].classList.add('active')
+                    break;
+                }
+            }
+    }
+    setInterval(() => {
+        addClass();
+    }, 10000);
+    containerImg.addEventListener('click', selectEvent);
 })();
